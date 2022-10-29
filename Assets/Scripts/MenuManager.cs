1c1291aca3929345ca0c7a3d86d098c8b53d68ae
laakar <laarka1102@gmail.com>
2022-10-29 14:54:48 -0300

    Revert "Revert "Audio listo""

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,12 +6,6 @@ using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-
-    public void SetVolume (float v)
-    {
-        audioMixer.SetFloat("volume", v);
-    }
     public void NextScene()
     {
         SceneManager.LoadScene("Juego");
