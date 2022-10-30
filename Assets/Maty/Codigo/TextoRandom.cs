1085ae0e5aa41e9ce8452402c81a2af944a2950f
laakar <laarka1102@gmail.com>
2022-10-29 21:13:10 -0300

    script tele

---
+++ Assets/Maty/Codigo/TextoRandom.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextoRandom : MonoBehaviour
+{
+    public TextMeshProUGUI[] randomText;
+    public TextMeshProUGUI randomTextObject;
+    public TextMeshProUGUI showText;
+
+    public bool showingText;
+
+    public void ShowRandomText()
+    {
+        RandomText();
+        showText = randomTextObject;
+    }
+
+    void RandomText()
+    {
+        int textPos = Random.Range(0, randomText.Length);
+        randomTextObject = randomText[textPos];
+    }
+}
